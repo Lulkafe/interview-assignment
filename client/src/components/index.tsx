25a0b81f95f7acc2c449e208daff30d5068f0407
Seimei Matsusaki <seimatsusaki@gmail.com>
2022-06-04 13:02:08 -0400

    replace createRoot => render with just render

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { render } from "react-dom";
 import { App } from "./app";
 import "normalize.css";
 import "../styles/global.sass";
 
 const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App/>);
+render(<App/>, container);
