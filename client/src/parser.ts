c231fea15558744e1e4afaa2856c7905229ac637
Lulkafe <seimatsusaki@gmail.com>
2022-06-05 17:38:54 +0900

    rename and add a new extract function

--- client/src/parser.ts
+++ client/src/parser.ts
@@ -1,21 +1,31 @@
-import { GeoCoordinates, WeatherInfo } from "./sharedTypes";
+import { CityInfo, GeoCoordinates, WeatherInfo } from "./sharedTypes";
 
 
-export const parseGeoData = (data): GeoCoordinates => {
+export const extractGeoCoordinates = (data): GeoCoordinates => {
     return {
         lat: data.lat,
         lon: data.lon
     }
 }
 
-export const parseWeatherData = (data): WeatherInfo => {
+export const extractCityInfo = (data): CityInfo => {
+    let cityInfo = {
+        name: data.name,
+        country: data.country
+    }
+
+    if (data.state) 
+        cityInfo["state"] = data.state;
+
+    return cityInfo;
+}
+
+
+export const extractWeatherData = (data): WeatherInfo => {
     return {
         name: data.name,
         country: data.sys.country,
-        temp: {
-            max: data.main.temp_max,
-            min: data.main.temp_min
-        },
+        temp: data.main.temp,
         weather: {
             current: data.weather[0].main,
             description: data.weather[0].description
